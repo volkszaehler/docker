4b0a15417eb6ebfbbb0717c8ca81a2d726712a83
andig <cpuidle@gmx.de>
2017-07-28 19:02:44 +0200

    Multi-arch

--- build.sh
+++ build.sh
@@ -5,9 +5,11 @@ TARGET=/to_host
 docker build -t build ./build 
 
 # prepare sources 
-docker run -v $(pwd)/dist:$TARGET build git clone https://github.com/volkszaehler/volkszaehler.org $TARGET
-docker run -v $(pwd)/dist:$TARGET build php composer.phar install -d $TARGET --optimize-autoloader --no-dev
+docker run -v $(pwd)/dist:$TARGET -t build git clone https://github.com/volkszaehler/volkszaehler.org $TARGET
+docker run -v $(pwd)/dist:$TARGET -t build php composer.phar install -d $TARGET --optimize-autoloader --no-dev
 
 # runtime image
+if [ -z $1 ] || [ "$1" !== "build" ] ; then
 docker build -t andig/volkszaehler:debian -f ./debian/Dockerfile .
 docker build -t andig/volkszaehler:raspbian -f ./raspbian/Dockerfile .
+fi
